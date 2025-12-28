17a62b7ab0967232531b71fccd9c60906e593660
ganesh14u <paladugusaiganesh@gmail.com>
2025-12-29 01:13:16 +0530

    Ser Fixed

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(null, false);
-  },
+  origin: true,
   credentials: true
 }));
 
